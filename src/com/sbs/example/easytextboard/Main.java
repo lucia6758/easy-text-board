561a4463c3c5c3ed4956ca434a24539537ee47bc
lucia6758 <lucia6758@gmail.com>
2020-10-17 17:36:16 +0900

    게시물 2개까지 저장할 수 있는 기능

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -6,6 +6,14 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
+		int article1_id = 0;
+		String article1_title = "";
+		String article1_body = "";
+		
+		int article2_id = 0;
+		String article2_title = "";
+		String article2_body = "";
+		
 		int lastArticleId = 0;
 
 		while (true) {
@@ -26,10 +34,33 @@ public class Main {
 				System.out.printf("제목 : %s\n" , title);
 				System.out.printf("내용 : %s\n" , body);
 				
+				if (id == 1) {
+					article1_id = id;
+					article1_title = title;
+					article1_body = body;
+				} else if (id == 2) {
+					article2_id = id;
+					article2_title = title;
+					article2_body = body;
+				}
+				
 				lastArticleId = id;
 				
 			} else if (command.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");
+				
+				if (lastArticleId == 0) {
+					System.out.println("게시물이 없습니다");
+					continue;
+				}
+				System.out.println("번호 / 제목");
+				
+				if (lastArticleId >= 1) {
+					System.out.printf("%d / %s\n", article1_id, article1_title );
+				}
+				if (lastArticleId >= 2) {
+					System.out.printf("%d / %s\n", article2_id, article2_title);
+				}
 			} else if (command.equals("system exit")) {
 				System.out.println("== 프로그램 종료 ==");
 				break;
