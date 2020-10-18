f0d72439b8ea9397fab8129abc70e41324a6846b
lucia6758 <lucia6758@gmail.com>
2020-10-18 16:37:09 +0900

    getArticle 메소드를 만들어서 if문 제거

--- src/com/sbs/example/easytextboard/APP.java
+++ src/com/sbs/example/easytextboard/APP.java
@@ -4,12 +4,20 @@ import java.util.Scanner;
 
 public class APP {
 
+	Article article1 = new Article();
+	Article article2 = new Article();
+
+	public Article getArticle(int id) {
+		if (id == 1) {
+			return article1;
+		} else if (id == 2) {
+			return article2;
+		}
+		return null;
+	}
+
 	public void run() {
 		Scanner scanner = new Scanner(System.in);
-
-		Article article1 = new Article();
-
-		Article article2 = new Article();
 
 		int lastArticleId = 0;
 
@@ -27,19 +35,15 @@ public class APP {
 				int id = lastArticleId + 1;
 
 				System.out.println("== 생성된 게시물 정보 ==");
-				System.out.printf("번호 : %d\n", id);
 				System.out.printf("제목 : %s\n", title);
 				System.out.printf("내용 : %s\n", body);
+				System.out.printf("%d번 게시물이 생성되었습니다.\n", id);
 
-				if (id == 1) {
-					article1.id = id;
-					article1.title = title;
-					article1.body = body;
-				} else if (id == 2) {
-					article2.id = id;
-					article2.title = title;
-					article2.body = body;
-				}
+				Article article = getArticle(id);
+
+				article.id = id;
+				article.title = title;
+				article.body = body;
 
 				lastArticleId = id;
 
@@ -52,36 +56,27 @@ public class APP {
 				}
 				System.out.println("번호 / 제목");
 
-				if (lastArticleId >= 1) {
-					System.out.printf("%d / %s\n", article1.id, article1.title);
+				for (int i = 1; i <= lastArticleId; i++) {
+					Article article = getArticle(i);
+
+					System.out.printf("%d / %s\n", article.id, article.title);
 				}
-				if (lastArticleId >= 2) {
-					System.out.printf("%d / %s\n", article2.id, article2.title);
-				}
+
 			} else if (command.startsWith("article detail ")) {
 				System.out.println("== 게시물 상세 ==");
 				int inputedId = Integer.parseInt(command.split(" ")[2]);
 
-				if (inputedId == 1) {
-					if (article1.id == 0) {
-						System.out.printf("%d번 게시물이 존재하지 않습니다.\n", inputedId);
-						continue;
-					}
-					System.out.printf("번호 : %d\n", article1.id);
-					System.out.printf("제목 : %s\n", article1.title);
-					System.out.printf("내용 : %s\n", article1.body);
-				} else if (inputedId == 2) {
-					if (article1.id == 0) {
-						System.out.printf("%d번 게시물이 존재하지 않습니다.\n", inputedId);
-						continue;
-					}
-					System.out.printf("번호 : %d\n", article2.id);
-					System.out.printf("제목 : %s\n", article2.title);
-					System.out.printf("내용 : %s\n", article2.body);
-				} else {
+				Article article = getArticle(inputedId);
+
+				if (lastArticleId == 0 || inputedId > lastArticleId) {
 					System.out.printf("%d번 게시물이 존재하지 않습니다.\n", inputedId);
 					continue;
 				}
+
+				System.out.printf("번호 : %d\n", article.id);
+				System.out.printf("제목 : %s\n", article.title);
+				System.out.printf("내용 : %s\n", article.body);
+
 			} else if (command.equals("system exit")) {
 				System.out.println("== 프로그램 종료 ==");
 				break;
