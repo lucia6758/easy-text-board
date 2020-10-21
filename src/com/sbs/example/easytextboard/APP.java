c5f9c1416bb2d864716a46fee55dc6b072fcc6c0
lucia6758 <lucia6758@gmail.com>
2020-10-21 22:33:24 +0900

    게시물 삭제

--- src/com/sbs/example/easytextboard/APP.java
+++ src/com/sbs/example/easytextboard/APP.java
@@ -4,23 +4,42 @@ import java.util.Scanner;
 
 public class APP {
 
-	Article[] articles = new Article[10];
+	Article[] articles = new Article[5];
 
 	int lastArticleId = 0;
 	int articlesSize = 0;
-	
+
 	int articlesSize() {
 		return articlesSize;
 	}
 
 	public Article getArticle(int id) {
-		if (id < 1) {
+		int index = getIndexById(id);
+
+		if (index == -1) {
 			return null;
 		}
-		if (id > lastArticleId) {
-			return null;
+
+		return articles[index];
+	}
+
+	private int getIndexById(int id) {
+		for (int i = 0; i < articlesSize(); i++) {
+			if (articles[i].id == id) {
+				return i;
+			}
 		}
-		return articles[id - 1];
+		return -1;
+	}
+	
+	private void removeArticle(int id) {
+		int index = getIndexById(id);
+		if ( index != -1 ){
+			for (int i = index+1; i < articlesSize(); i++) {
+				articles[i-1] = articles [i];
+			}
+		} 
+		articlesSize--;
 	}
 
 	public void run() {
@@ -40,7 +59,7 @@ public class APP {
 					System.out.println("더 이상 작성할 수 없습니다.");
 					continue;
 				}
-				
+
 				int id = lastArticleId + 1;
 				lastArticleId = id;
 				System.out.printf("제목 : ");
@@ -55,9 +74,9 @@ public class APP {
 				article.id = id;
 				article.title = title;
 				article.body = body;
-				
+
 				articles[articlesSize] = article;
-				
+
 				articlesSize++;
 
 			} else if (command.equals("article list")) {
@@ -69,8 +88,8 @@ public class APP {
 				}
 				System.out.println("번호 / 제목");
 
-				for (int i = 1; i <= lastArticleId; i++) {
-					Article article = getArticle(i);
+				for (int i = 0; i < articlesSize(); i++) {
+					Article article = articles[i];
 
 					System.out.printf("%d / %s\n", article.id, article.title);
 				}
@@ -89,6 +108,21 @@ public class APP {
 				System.out.printf("번호 : %d\n", article.id);
 				System.out.printf("제목 : %s\n", article.title);
 				System.out.printf("내용 : %s\n", article.body);
+
+			} else if (command.startsWith("article delete ")) {
+				System.out.println("== 게시물 삭제 ==");
+				int inputedId = Integer.parseInt(command.split(" ")[2]);
+
+				Article article = getArticle(inputedId);
+
+				if (article == null) {
+					System.out.printf("%d번 게시물이 존재하지 않습니다.\n", inputedId);
+					continue;
+				}
+				
+				removeArticle(inputedId);
+
+				System.out.printf("%d번 게시물이 삭제되었습니다.\n", inputedId);
 
 			} else if (command.equals("system exit")) {
 				System.out.println("== 프로그램 종료 ==");
